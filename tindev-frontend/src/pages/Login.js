2cc1a0558314bdb2d4e09a8ff5cce97918bce060
Guilherme Bezerra <gbsantos.it@gmail.com>
2019-08-07 11:50:19 -0300

    [TinDev-FrontEnd] Applyed State on Form Input

--- tindev-frontend/src/pages/Login.js
+++ tindev-frontend/src/pages/Login.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 
 import logo from '../assets/logo.svg';
 import './Login.css';
 
 export default function Login() {
+  const [username, setUsername] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(username);
+  }
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt="TinDev" />
-        <input placeholder="Digite o seu usuário do Github" />
+        <input
+          placeholder="Digite o seu usuário do Github"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
         <button type="submit">Enviar</button>
       </form>
     </div>
